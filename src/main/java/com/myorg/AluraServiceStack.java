e3051f7e346f49b99186fd2c3191d1c30e27a508
Leonardo Isaias <leoisaias135@gmail.com>
2025-04-08 23:07:08 -0300

    Adição da configuração para ser exibido os logs no CloudWatch

--- src/main/java/com/myorg/AluraServiceStack.java
+++ src/main/java/com/myorg/AluraServiceStack.java
@@ -1,15 +1,19 @@
 package com.myorg;
 
 import software.amazon.awscdk.Fn;
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.ecr.IRepository;
 import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
 import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.LogDriver;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.logs.LogGroup;
 import software.constructs.Construct;
 
 import java.util.HashMap;
@@ -26,7 +30,7 @@ public class AluraServiceStack extends Stack {
         super(scope, id, props);
 
         // Criação de um mapa para armazenar as informações de autenticação
-        Map<String, String> autenticacao= new HashMap<>();
+        Map<String, String> autenticacao = new HashMap<>();
         // Adiciona a URL do banco de dados ao mapa, concatenando o endpoint e a porta
         autenticacao.put("SPRING_DATASOURCE_URL", "jdbc:mysql://" + Fn.importValue("pedidos-db-endpoint") + ":3306/alurafood-pedidos?createDatabaseIfNotExist=true");
         // Adiciona o nome de usuário ao mapa
@@ -42,7 +46,7 @@ public class AluraServiceStack extends Stack {
                 .serviceName("alura-service-ola") // Nome do serviço
                 .cluster(cluster)                   // Referência ao cluster criado
                 .cpu(512)                           // Alocação de CPU para o serviço
-                .desiredCount(1)                    // Número desejado de instâncias do serviço
+                .desiredCount(3)                    // Número desejado de instâncias do serviço
                 .listenerPort(8080)                 // Porta que o serviço irá escutar
                 .assignPublicIp(true)               // Atribui um IP público ao serviço
                 .taskImageOptions(
@@ -52,6 +56,13 @@ public class AluraServiceStack extends Stack {
                                 .containerPort(8080) // Porta do container
                                 .containerName("app_ola") // Nome do container
                                 .environment(autenticacao) // Configura as variáveis de ambiente da aplicação com as informações de autenticação
+                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder() // Configura o driver de log para usar o AWS Logs
+                                        .logGroup(LogGroup.Builder.create(this, "PedidosMsLogGroup") // Cria um grupo de logs com o nome "PedidosMsLogGroup"
+                                                .logGroupName("PedidosMsLog") // Define o nome do grupo de logs como "PedidosMsLog"
+                                                .removalPolicy(RemovalPolicy.DESTROY) // Define a política de remoção para destruir os logs ao apagar a stack
+                                                .build())
+                                        .streamPrefix("PedidosMS") // Define um prefixo para os streams de log como "PedidosMS"
+                                        .build()))
                                 .build())
                 .memoryLimitMiB(1024)               // Limite de memória para o serviço
                 .publicLoadBalancer(true)           // Define que o load balancer é público
